2d0a7b40c9811f5d34cca92ce4abcb4039a63776
Markuff1 <marknixon1@outlook.com>
2025-08-05 21:32:06 +0100

    Fix route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/mark-cv" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/mark-cv" />} />
+          <Route path="/mark-cv" element={<Home />} />
+        </Routes>
     </Router>
   );
 }
